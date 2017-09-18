e741122a1b912d1dc6482443b84dd3ed56a7215d
lbdnuf@163.com <Dong131421>
2017-09-18 17:43:43 +0800

    测试idea提交代码

--- spring-cloud-01-eureka-server-a/src/main/java/bhz/spring/cloud/EurekaApplication.java
+++ spring-cloud-01-eureka-server-a/src/main/java/bhz/spring/cloud/EurekaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer //表示服务发现中心的配置启用
+@EnableEurekaServer //表示服务发现中心的配置启用//测是idea提交代码
 @SpringBootApplication
 public class EurekaApplication {
 
